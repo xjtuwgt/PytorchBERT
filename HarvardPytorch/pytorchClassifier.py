cbaebc82db2c2c2ebd23745ffd4e6837ba396413
guangtao.wang <guangtao.wang@jd.com>
2018-12-06 20:14:46 -0800

    GPU+

--- HarvardPytorch/pytorchClassifier.py
+++ HarvardPytorch/pytorchClassifier.py
@@ -18,12 +18,12 @@ print(transform)
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=256,
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32,
                                           shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=256,
+testloader = torch.utils.data.DataLoader(testset, batch_size=32,
                                          shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
